678982a83c31595e265d7b60dcc1eccede474411
Stoia <alin58236@gmail.com>
2022-10-19 02:26:34 +0300

    Add project files.

---
+++ Data/Stoia_Alin_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stoia_Alin_Lab2.Models;
+
+namespace Stoia_Alin_Lab2.Data
+{
+    public class Stoia_Alin_Lab2Context : DbContext
+    {
+        public Stoia_Alin_Lab2Context (DbContextOptions<Stoia_Alin_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stoia_Alin_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,11 @@
+﻿namespace Stoia_Alin_Lab2.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+
+        public string PublisherName { get; set; }
+
+        public ICollection<Book>? Books { get; set; }
+    }
+}
